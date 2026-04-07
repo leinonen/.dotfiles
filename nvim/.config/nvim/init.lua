0db813096b179e072307202b44792435235af518
Peter Leinonen <peter@leinonen.se>
2026-04-07 19:10:23 +0200

    refactor(nvim): migrate from lazy.nvim to vim.pack

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,47 +2,76 @@
 vim.g.mapleader = " "
 
 local o = vim.opt
-o.number         = true
+o.number = true
 o.relativenumber = true
-o.signcolumn     = "yes"
-o.cursorline     = true
-o.tabstop        = 4
-o.shiftwidth     = 4
-o.expandtab      = false  -- Go uses real tabs
-o.termguicolors  = true
-o.completeopt    = { "menu", "menuone", "noselect" }
+o.signcolumn = "yes"
+o.cursorline = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.expandtab = false -- Go uses real tabs
+o.termguicolors = true
+o.completeopt = { "menu", "menuone", "noselect" }
 
 vim.cmd.colorscheme("retrobox")
 
 require("statusline").setup()
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable",
-    "https://github.com/folke/lazy.nvim.git", lazypath })
+vim.pack.add({
+  { src = "https://github.com/nvim-lua/plenary.nvim", name = "plenary.nvim" },
+  { src = "https://github.com/nvim-telescope/telescope.nvim", name = "telescope.nvim" },
+  { src = "https://github.com/mfussenegger/nvim-dap", name = "nvim-dap" },
+}, { load = false, confirm = false })
+
+local loaded_plugins = {}
+
+local function load_plugin(name)
+  if loaded_plugins[name] then
+    return
+  end
+
+  vim.cmd.packadd(name)
+  loaded_plugins[name] = true
 end
-vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  { "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" } },
-  { "mfussenegger/nvim-dap" },
-})
+local function with_plugin(name, callback)
+  return function(...)
+    load_plugin(name)
+    return callback(...)
+  end
+end
 
-require("telescope").setup({
-  pickers = {
-    colorscheme = {
-      enable_preview = true,
+local telescope_ready = false
+
+local function ensure_telescope()
+  load_plugin("telescope.nvim")
+
+  if telescope_ready then
+    return
+  end
+
+  require("telescope").setup({
+    pickers = {
+      colorscheme = {
+        enable_preview = true,
+      },
     },
-  },
-})
+  })
+
+  telescope_ready = true
+end
+
+local function telescope_builtin(name)
+  return with_plugin("telescope.nvim", function()
+    ensure_telescope()
+    require("telescope.builtin")[name]()
+  end)
+end
 
 -- Built-in LSP (Neovim 0.11+)
 vim.lsp.config("gopls", {
-  cmd       = { "gopls" },
+  cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork" },
-  settings  = { gopls = { gofumpt = true, staticcheck = true } },
+  settings = { gopls = { gofumpt = true, staticcheck = true } },
 })
 vim.lsp.enable("gopls")
 
@@ -52,12 +81,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local m = function(k, f) vim.keymap.set("n", k, f, { buffer = ev.buf }) end
     vim.lsp.completion.enable(true, ev.data.client_id, ev.buf, { autotrigger = true })
     vim.keymap.set("i", "<C-Space>", function() vim.lsp.completion.trigger() end, { buffer = ev.buf })
-    vim.keymap.set("i", "<C-e>",     "<C-x><C-z>",               { buffer = ev.buf })
-    m("gd",         b.definition)
-    m("K",          b.hover)
+    vim.keymap.set("i", "<C-e>", "<C-x><C-z>", { buffer = ev.buf })
+    m("gd", b.definition)
+    m("K", b.hover)
     m("<leader>rn", b.rename)
     m("<leader>ca", b.code_action)
-    m("<leader>f",  function() b.format({ async = true }) end)
+    m("<leader>f", function() b.format({ async = true }) end)
   end,
 })
 
@@ -69,52 +98,62 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
 })
 
--- DAP / Delve
-local dap = require("dap")
+local dap_ready = false
 
-dap.adapters.go = {
-  type = "server",
-  port = "${port}",
-  executable = { command = "dlv", args = { "dap", "-l", "127.0.0.1:${port}" } },
-}
+local function ensure_dap()
+  load_plugin("nvim-dap")
 
-dap.configurations.go = {
-  { type = "go", name = "Debug",            request = "launch", program = "${file}" },
-  { type = "go", name = "Debug package",    request = "launch", program = "${fileDirname}" },
-  { type = "go", name = "Debug test",       request = "launch", program = "${file}", mode = "test" },
-  { type = "go", name = "Attach",           request = "attach", processId = require("dap.utils").pick_process },
-}
+  if dap_ready then
+    return require("dap")
+  end
 
-local m = function(k, f, d) vim.keymap.set("n", k, f, { desc = d }) end
-m("<F5>",        dap.continue,          "Debug: continue")
-m("<F10>",       dap.step_over,         "Debug: step over")
-m("<F11>",       dap.step_into,         "Debug: step into")
-m("<F12>",       dap.step_out,          "Debug: step out")
-m("<leader>db",  dap.toggle_breakpoint, "Debug: toggle breakpoint")
-m("<leader>dr",  dap.repl.open,         "Debug: open REPL")
+  local dap = require("dap")
+
+  dap.adapters.go = {
+    type = "server",
+    port = "${port}",
+    executable = { command = "dlv", args = { "dap", "-l", "127.0.0.1:${port}" } },
+  }
+
+  dap.configurations.go = {
+    { type = "go", name = "Debug", request = "launch", program = "${file}" },
+    { type = "go", name = "Debug package", request = "launch", program = "${fileDirname}" },
+    { type = "go", name = "Debug test", request = "launch", program = "${file}", mode = "test" },
+    { type = "go", name = "Attach", request = "attach", processId = require("dap.utils").pick_process },
+  }
+
+  dap_ready = true
+
+  return dap
+end
+
+local map = function(k, f, d) vim.keymap.set("n", k, f, { desc = d }) end
+map("<F5>", with_plugin("nvim-dap", function() ensure_dap().continue() end), "Debug: continue")
+map("<F10>", with_plugin("nvim-dap", function() ensure_dap().step_over() end), "Debug: step over")
+map("<F11>", with_plugin("nvim-dap", function() ensure_dap().step_into() end), "Debug: step into")
+map("<F12>", with_plugin("nvim-dap", function() ensure_dap().step_out() end), "Debug: step out")
+map("<leader>db", with_plugin("nvim-dap", function() ensure_dap().toggle_breakpoint() end), "Debug: toggle breakpoint")
+map("<leader>dr", with_plugin("nvim-dap", function() ensure_dap().repl.open() end), "Debug: open REPL")
 
 -- Go run / test
 local function term(cmd)
   vim.cmd("split | terminal " .. cmd)
 end
- 
-vim.keymap.set("n", "<leader>tr", function() term("go run ./...") end,          { desc = "go run" })
-vim.keymap.set("n", "<leader>tt", function() term("go test ./...") end,         { desc = "go test" })
-vim.keymap.set("n", "<leader>tv", function() term("go test -v ./...") end,      { desc = "go test -v" })
-vim.keymap.set("n", "<leader>tc", function() term("go test -cover ./...") end,  { desc = "go test -cover" })
+
+vim.keymap.set("n", "<leader>tr", function() term("go run ./...") end, { desc = "go run" })
+vim.keymap.set("n", "<leader>tt", function() term("go test ./...") end, { desc = "go test" })
+vim.keymap.set("n", "<leader>tv", function() term("go test -v ./...") end, { desc = "go test -v" })
+vim.keymap.set("n", "<leader>tc", function() term("go test -cover ./...") end, { desc = "go test -cover" })
 vim.keymap.set("n", "<leader>tl", function() term("go test -run " .. vim.fn.expand("<cword>") .. " ./...") end, { desc = "go test func under cursor" })
 
 -- Telescope
-local t = require("telescope.builtin")
-
--- files
-vim.keymap.set("n", "<leader><leader>", t.find_files)
-vim.keymap.set("n", "<leader>/",        t.live_grep)
-vim.keymap.set("n", "<leader>b",        t.buffers)
+vim.keymap.set("n", "<leader><leader>", telescope_builtin("find_files"))
+vim.keymap.set("n", "<leader>/", telescope_builtin("live_grep"))
+vim.keymap.set("n", "<leader>b", telescope_builtin("buffers"))
 
 -- lsp
-vim.keymap.set("n", "<leader>d",  t.diagnostics)
-vim.keymap.set("n", "<leader>s",  t.lsp_document_symbols)
-vim.keymap.set("n", "<leader>S",  t.lsp_workspace_symbols)
-vim.keymap.set("n", "gr",         t.lsp_references)
-vim.keymap.set("n", "gi",         t.lsp_implementations)
+vim.keymap.set("n", "<leader>d", telescope_builtin("diagnostics"))
+vim.keymap.set("n", "<leader>s", telescope_builtin("lsp_document_symbols"))
+vim.keymap.set("n", "<leader>S", telescope_builtin("lsp_workspace_symbols"))
+vim.keymap.set("n", "gr", telescope_builtin("lsp_references"))
+vim.keymap.set("n", "gi", telescope_builtin("lsp_implementations"))
